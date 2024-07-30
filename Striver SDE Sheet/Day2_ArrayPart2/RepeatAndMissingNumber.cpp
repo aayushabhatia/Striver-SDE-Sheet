c6836f8583b6aae5a216232190d1fb16d06a0323
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2024-07-30 19:21:18 +0530

    Create RepeatAndMissingNumber.cpp

---
+++ Striver SDE Sheet/Day2_ArrayPart2/RepeatAndMissingNumber.cpp
@@ -0,0 +1,6 @@
+// Repeat and Missing number in array
+// https://www.interviewbit.com/problems/repeat-and-missing-number-array/
+// https://takeuforward.org/data-structure/find-the-repeating-and-missing-numbers/
+
+#include<bits/stdc++.h>
+using namespace std;
