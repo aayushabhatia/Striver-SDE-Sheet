38fa06fef48c952434ecb8f59c648e341bd97cf5
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2024-07-31 15:55:23 +0530

    Update 2DMatrixSearch.cpp

--- Striver SDE Sheet/Day3_ArrayPart3/2DMatrixSearch.cpp
+++ Striver SDE Sheet/Day3_ArrayPart3/2DMatrixSearch.cpp
@@ -5,6 +5,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Brute Force
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
@@ -34,5 +35,25 @@ public:
         }
         
         return false;
+    }
+};
+
+// Optimal Solution- Hypothetically flatten the array
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    //apply binary search:
+    int low = 0, high = n * m - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int row = mid / m, col = mid % m;
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
     }
 };
