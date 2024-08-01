6b75a207888ce8485809398b5ec9be4b64ab64ff
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2024-08-01 18:37:16 +0530

    Create LongestConsecutiveSum.cpp

---
+++ Striver SDE Sheet/Day4_ArrayPart4/LongestConsecutiveSum.cpp
@@ -0,0 +1,34 @@
+// 128. Longest Consecutive Sum
+// https://leetcode.com/problems/longest-consecutive-sequence/
+// https://takeuforward.org/data-structure/longest-consecutive-sequence-in-an-array/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+    if (n == 0) return 0;
+    sort(nums.begin(), nums.end());
+    // Initializing
+    int lastSmaller = INT_MIN;
+    int cnt = 0;
+    int longest = 1;
+
+    //find longest sequence:
+    for (int i = 0; i < n; i++) {
+        if (nums[i] - 1 == lastSmaller) { // lastSmaller is the previous element and if current element value -1 is equal to lastsmaller then inc the cnt ie the length
+            //a[i] is the next element of the current sequence.
+            cnt += 1;
+            lastSmaller = nums[i];
+        }
+        else if (nums[i] != lastSmaller) {
+            cnt = 1;
+            lastSmaller = nums[i];
+        }
+        longest = max(longest, cnt);
+    }
+    return longest;
+    }
+};
