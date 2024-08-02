cd1505df6d4a2e37e80b7f47a5607b0f0b026cf2
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2024-08-02 19:17:29 +0530

    Add files via upload

---
+++ Striver SDE Sheet/Day5_LinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,32 @@
+// Reverse a Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+// https://takeuforward.org/data-structure/reverse-a-linked-list/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// DSA Udemy solution
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* c= head;
+        ListNode* p= NULL;
+        ListNode* N;
+        while(c!=NULL){
+            N= c->next;
+            c->next= p;
+            p=c;
+            c=N;
+        }
+        head=p;
+        return head;
+    }
+};
