1b425a1ba12df895dd9da1d01cbd49e59b036425
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2024-08-02 16:52:18 +0530

    Create CountSubarraysWithXORk.cpp

---
+++ Striver SDE Sheet/Day4_ArrayPart4/CountSubarraysWithXORk.cpp
@@ -0,0 +1,6 @@
+// Count number of subarrays with XOR K
+// https://www.interviewbit.com/problems/subarray-with-given-xor/
+// https://takeuforward.org/data-structure/count-the-number-of-subarrays-with-given-xor-k/
+
+#include<bits/stdc++.h>
+using namespace std;
