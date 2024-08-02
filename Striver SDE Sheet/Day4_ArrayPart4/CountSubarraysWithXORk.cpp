af2747df3a28a8463309c7d7fe9aef963db4aa5b
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2024-08-02 17:33:00 +0530

    Update CountSubarraysWithXORk.cpp

--- Striver SDE Sheet/Day4_ArrayPart4/CountSubarraysWithXORk.cpp
+++ Striver SDE Sheet/Day4_ArrayPart4/CountSubarraysWithXORk.cpp
@@ -4,3 +4,25 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+
+int Solution::solve(vector<int> &A, int k) {
+    int n = A.size(); //size of the given array.
+    int xr = 0;
+    map<int, int> mpp; //declaring the map.
+    mpp[xr]++; //setting the value of 0.
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        // prefix XOR till index i:
+        xr = xr ^ A[i];
+
+        //By formula: x = xr^k:
+        int x = xr ^ k;
+
+        // add the occurrence of xr^k to the count:
+        cnt += mpp[x];
+
+        // Insert the prefix xor till index i into the map:
+        mpp[xr]++;
+    }
+    return cnt;
+}
