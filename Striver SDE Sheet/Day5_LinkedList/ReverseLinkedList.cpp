8999529957bfcb4dbb4df50efdb497e37dde2597
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2024-08-07 10:58:14 +0530

    Update ReverseLinkedList.cpp

--- Striver SDE Sheet/Day5_LinkedList/ReverseLinkedList.cpp
+++ Striver SDE Sheet/Day5_LinkedList/ReverseLinkedList.cpp
@@ -17,16 +17,16 @@ struct ListNode {
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* c= head;
-        ListNode* p= NULL;
+        ListNode* c = head;
+        ListNode* p = NULL;
         ListNode* N;
-        while(c!=NULL){
-            N= c->next;
-            c->next= p;
-            p=c;
-            c=N;
+        while(c != NULL){
+            N = c-> next;
+            c -> next = p;
+            p = c;
+            c = N;
         }
-        head=p;
+        head = p;
         return head;
     }
 };
