6ce8a680991aabb1a424fdd5920c48f11e4a2f3c
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2024-08-07 11:31:46 +0530

    Create MergeTwoSortedLinkedList.cpp

---
+++ Striver SDE Sheet/Day5_LinkedList/MergeTwoSortedLinkedList.cpp
@@ -0,0 +1,45 @@
+// Merge Two sorted linked lists
+// https://takeuforward.org/data-structure/merge-two-sorted-linked-lists/
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // Create a dummy node to serve as the head of the merged list
+    ListNode* dummyNode = new ListNode(-1);
+    ListNode* temp = dummyNode;
+
+    // Traverse both lists simultaneously
+    while (list1 != nullptr && list2 != nullptr) {
+        // Compare elements of both lists and link the smaller node to the merged list
+        if (list1->val <= list2->val) {
+            temp->next = list1;
+            list1 = list1->next;
+        } else {
+            temp->next = list2;
+            list2 = list2->next;
+        }
+        // Move the temporary pointer to the next node
+        temp = temp->next; 
+    }
+
+    // If any list still has remaining elements, append them to the merged list
+    if (list1 != nullptr) {
+        temp->next = list1;
+    } else {
+        temp->next = list2;
+    }
+    // Return the merged list starting from the next of the dummy node
+    return dummyNode->next;
+    }
+};
