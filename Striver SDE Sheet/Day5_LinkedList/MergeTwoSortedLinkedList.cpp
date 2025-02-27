f66b214589627a4466de2e8a88ef53e2fb74abf2
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-02-27 21:10:36 +0530

    Update MergeTwoSortedLinkedList.cpp

--- Striver SDE Sheet/Day5_LinkedList/MergeTwoSortedLinkedList.cpp
+++ Striver SDE Sheet/Day5_LinkedList/MergeTwoSortedLinkedList.cpp
@@ -12,34 +12,33 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // Create a dummy node to serve as the head of the merged list
-    ListNode* dummyNode = new ListNode(-1);
-    ListNode* temp = dummyNode;
+        ListNode* dNode = new ListNode(-1);
+        ListNode* temp = dNode;
 
-    // Traverse both lists simultaneously
-    while (list1 != nullptr && list2 != nullptr) {
-        // Compare elements of both lists and link the smaller node to the merged list
-        if (list1->val <= list2->val) {
-            temp->next = list1;
-            list1 = list1->next;
-        } else {
-            temp->next = list2;
-            list2 = list2->next;
+        while(list1!= NULL && list2!= NULL){
+            if (list1->val <= list2->val) {
+                temp->next = list1;
+                temp = list1;
+                list1 = list1->next;
+            } 
+            else {
+                temp->next = list2;
+                temp = list2;
+                list2 = list2->next;
+            } 
         }
-        // Move the temporary pointer to the next node
-        temp = temp->next; 
-    }
 
-    // If any list still has remaining elements, append them to the merged list
-    if (list1 != nullptr) {
-        temp->next = list1;
-    } else {
-        temp->next = list2;
-    }
-    // Return the merged list starting from the next of the dummy node
-    return dummyNode->next;
+        if (list1 != NULL) {
+            temp->next = list1;
+        } 
+        else {
+            temp->next = list2;
+        }
+        
+        return dNode->next;
     }
 };
