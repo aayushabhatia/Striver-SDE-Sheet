d3f3e7a176ff41242ec5f2c50af4327df45fd160
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-03-02 22:07:45 +0530

    Update Extra_3SumProblem.cpp

--- Striver SDE Sheet/Day4_ArrayPart4/Extra_3SumProblem.cpp
+++ Striver SDE Sheet/Day4_ArrayPart4/Extra_3SumProblem.cpp
@@ -1,3 +1,28 @@
 // 3 Sum Problem
 // https://leetcode.com/problems/3sum/description/
 // https://takeuforward.org/data-structure/3-sum-find-triplets-that-add-up-to-a-zero/
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n= nums.size();
+        set<vector<int>> st;
+
+    // check all possible triplets:
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+
+    //store the set elements in the answer:
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+    }
+};
