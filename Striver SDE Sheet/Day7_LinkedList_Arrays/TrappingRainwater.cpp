49997ca34a2d7491f1569399fbad306010f7a65c
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-03-04 02:18:38 +0530

    Update TrappingRainwater.cpp

--- Striver SDE Sheet/Day7_LinkedList_Arrays/TrappingRainwater.cpp
+++ Striver SDE Sheet/Day7_LinkedList_Arrays/TrappingRainwater.cpp
@@ -7,22 +7,20 @@ using namespace std;
 
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-        int n = arr.size();
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int prefix[n], suffix[n];
+        prefix[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = max(prefix[i - 1], height[i]);
+        }
+        suffix[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = max(suffix[i + 1], height[i]);
+        }
         int waterTrapped = 0;
         for (int i = 0; i < n; i++) {
-            int j = i;
-            int leftMax = 0, rightMax = 0;
-            while (j >= 0) {
-                leftMax = max(leftMax, arr[j]);
-                j--;
-            }
-            j = i;
-            while (j < n) {
-                rightMax = max(rightMax, arr[j]);
-                j++;
-            }
-            waterTrapped += min(leftMax, rightMax) - arr[i];
+            waterTrapped += min(prefix[i], suffix[i]) - height[i];
         }
         return waterTrapped;
     }
