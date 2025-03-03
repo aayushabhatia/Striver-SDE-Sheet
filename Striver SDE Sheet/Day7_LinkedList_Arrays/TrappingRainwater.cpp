d17a495ca383da3e262c1f634d98a5406a88489a
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-03-03 12:48:55 +0530

    Create TrappingRainwater.cpp

---
+++ Striver SDE Sheet/Day7_LinkedList_Arrays/TrappingRainwater.cpp
@@ -0,0 +1,7 @@
+// Trapping Rainwater
+// https://leetcode.com/problems/trapping-rain-water/description/
+// https://takeuforward.org/data-structure/trapping-rainwater/
+
+#include<bits/stdc++.h>
+using namespace std;
+
