d53b23e5f336dafbfda26447efc7cb8a05a1b763
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-04-19 14:07:25 +0530

    Update PalindromePartitioning.cpp

--- Striver SDE Sheet/Day9_Recursion/PalindromePartitioning.cpp
+++ Striver SDE Sheet/Day9_Recursion/PalindromePartitioning.cpp
@@ -1,1 +1,33 @@
+// https://leetcode.com/problems/palindrome-partitioning/
+// https://takeuforward.org/data-structure/palindrome-partitioning/
 
+class Solution {
+public:
+    void func(int ind, string s, vector<string> &ds, vector<vector<string>> &ans){
+        if(ind == s.size()){ // base case
+            ans.push_back(ds);
+            return;
+        }
+        for (int i = ind; i < s.size(); ++i) {
+            if(isPalindrome(s,ind,i)){
+                ds.push_back(s.substr(ind, i-ind+1)); // s.substr(start, length) , i - ind + 1 is the length of the substring from ind to i
+                func(i+1, s, ds, ans);
+                ds.pop_back();
+            }
+        }
+    }
+    bool isPalindrome(string s, int start, int end){
+        while(start<= end){
+            if(s[start++]!= s[end--]){
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> ds;
+        func(0, s, ds, ans);
+        return ans;
+    }
+};
