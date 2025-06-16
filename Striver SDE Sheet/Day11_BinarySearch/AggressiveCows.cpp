21d4fa5744744c4be91019e344a24ed6e9e1c523
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-06-16 18:52:58 +0530

    Update AggressiveCows.cpp

--- Striver SDE Sheet/Day11_BinarySearch/AggressiveCows.cpp
+++ Striver SDE Sheet/Day11_BinarySearch/AggressiveCows.cpp
@@ -1,3 +1,35 @@
 // https://takeuforward.org/data-structure/aggressive-cows-detailed-solution/
 // https://www.geeksforgeeks.org/problems/aggressive-cows/1
 
+class Solution {
+  public:
+    bool canWePlace(vector<int> &stalls, int dist, int cows) {
+        int n = stalls.size(); //size of array
+        int cntCows = 1; //no. of cows placed
+        int last = stalls[0]; //position of last placed cow.
+        for (int i = 1; i < n; i++) {
+            if (stalls[i] - last >= dist) {
+                cntCows++; //place next cow.
+                last = stalls[i]; //update the last location.
+            }
+            if (cntCows >= cows) return true;
+        }
+        return false;
+    }
+    
+    int aggressiveCows(vector<int> &stalls, int k) {
+        int n = stalls.size(); //size of array
+        sort(stalls.begin(), stalls.end());
+        
+        int low = 1, high = stalls[n - 1] - stalls[0];   
+        //apply binary search
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (canWePlace(stalls, mid, k) == true) {
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return high;
+    }
+};
