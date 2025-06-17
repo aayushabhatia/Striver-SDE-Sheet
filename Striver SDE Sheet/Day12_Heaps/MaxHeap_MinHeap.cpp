f54ec7c7c949b0c5d7ebbab8e40ec5655edfe993
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-06-17 20:45:28 +0530

    Update MaxHeap_MinHeap.cpp

--- Striver SDE Sheet/Day12_Heaps/MaxHeap_MinHeap.cpp
+++ Striver SDE Sheet/Day12_Heaps/MaxHeap_MinHeap.cpp
@@ -1,3 +1,5 @@
 priority_queue<int, vector<int>, greater<int>> minh; // way to define min heap
 priority_queue<int> maxh;   // way to define max heap
 // heap used when in ques there is k and smallest/largest is asked
+// max heap used in case of smallest, min heap used in case of largest
+// from sorting we get time complexity of nlogn (merge sort), its optimised approach is using heap which gives nlogk time complexity where k is a given number in ques.
