3a9009e1c21246ec1d8f7d18a6e5890c197a8a89
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-06-17 19:32:36 +0530

    Update MaxHeap_MinHeap.cpp

---
+++ Striver SDE Sheet/Day12_Heaps/MaxHeap_MinHeap.cpp
@@ -0,0 +1,3 @@
+priority_queue<int, vector<int>, greater<int>> minh; // way to define min heap
+priority_queue<int> maxh;   // way to define max heap
+// heap used when in ques there is k and smallest/largest is asked
