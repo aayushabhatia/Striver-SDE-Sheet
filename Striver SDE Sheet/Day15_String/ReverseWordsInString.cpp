ef976d62e9c718b853780c4a007dc87a1008034f
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-06-22 14:06:14 +0530

    Update ReverseWordsInString.cpp

---
+++ Striver SDE Sheet/Day15_String/ReverseWordsInString.cpp
@@ -0,0 +1,25 @@
+// https://takeuforward.org/data-structure/reverse-words-in-a-string/
+// https://leetcode.com/problems/reverse-words-in-a-string/description/
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans = "";
+        int n = s.size();
+        for(int i = 0 ; i < s.size() ; i++){
+            string temp;
+            while(s[i]==' ' && i < n){
+                i++;
+            }
+            while(s[i] != ' ' && i < n){
+                temp += s[i];
+                i++;
+            }
+            if(temp.size()>0){
+                if(ans.empty()) ans = temp;
+                else ans = temp + " " + ans;
+            }
+        }
+        return ans;   
+    }
+};
