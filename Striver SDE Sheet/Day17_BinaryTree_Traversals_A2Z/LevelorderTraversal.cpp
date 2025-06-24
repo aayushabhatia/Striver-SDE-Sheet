bd16fd8de6cea60ca49ffed59f1a551869813dc8
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-06-24 23:32:01 +0530

    Create LevelorderTraversal.cpp

---
+++ Striver SDE Sheet/Day17_BinaryTree_Traversals_A2Z/LevelorderTraversal.cpp
@@ -0,0 +1,27 @@
+// https://takeuforward.org/data-structure/level-order-traversal-of-a-binary-tree/
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL){
+            return ans;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while(q.size()>0){
+            int size= q.size();
+            vector<int> level;
+            for(int i=0; i<size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left!=NULL) q.push(node->left);
+                if(node->right!=NULL) q.push(node->right);
+                level.push_back(node->val);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
