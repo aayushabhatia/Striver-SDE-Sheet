f41e0f4dbb9f13aab5a9b132fbb7b449fe2dd95c
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-06-24 19:59:19 +0530

    Create PostorderTraversal.cpp

---
+++ Striver SDE Sheet/Day17_BinaryTree_Traversals_A2Z/PostorderTraversal.cpp
@@ -0,0 +1,19 @@
+// https://takeuforward.org/data-structure/post-order-traversal-of-binary-tree/
+// https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+class Solution {
+public:
+    void postorder(TreeNode* root, vector<int> &arr){
+        if(root==NULL){
+            return;
+        }
+        postorder(root->left, arr);
+        postorder(root->right, arr);
+        arr.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> arr;
+        postorder(root, arr);
+        return arr;
+    }
+};
