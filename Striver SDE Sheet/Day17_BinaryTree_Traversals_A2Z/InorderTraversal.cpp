b20a6c4d46ec61dc4c2407b4be52d1db3180bc2b
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-06-25 20:26:54 +0530

    Update InorderTraversal.cpp

--- Striver SDE Sheet/Day17_BinaryTree_Traversals_A2Z/InorderTraversal.cpp
+++ Striver SDE Sheet/Day17_BinaryTree_Traversals_A2Z/InorderTraversal.cpp
@@ -1,6 +1,7 @@
 // https://takeuforward.org/data-structure/inorder-traversal-of-binary-tree/
 // https://leetcode.com/problems/binary-tree-inorder-traversal/description/
 
+// Recursive
 class Solution {
 public:
     void inorder(TreeNode* root, vector<int> &arr){
@@ -15,5 +16,34 @@ public:
         vector<int> arr;
         inorder(root, arr);
         return arr;
+    }
+};
+
+
+// Iterative
+// https://www.youtube.com/watch?v=lxTGsVXjwvM&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=11
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        TreeNode* node = root;
+        vector<int> inorder_traversal;
+        while(true){
+            if(node!=NULL){
+                s.push(node);
+                node = node->left;
+            }
+            else{
+                if(s.size()==0){
+                    break;
+                }
+                node=s.top();
+                s.pop();
+                inorder_traversal.push_back(node->val);
+                node= node->right;
+            }
+        }
+        return inorder_traversal;
     }
 };
