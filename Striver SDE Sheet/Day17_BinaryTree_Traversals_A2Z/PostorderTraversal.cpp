9e01faab529b9a3ca5dedab95da909d120527a40
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-06-26 00:47:33 +0530

    Update PostorderTraversal.cpp

--- Striver SDE Sheet/Day17_BinaryTree_Traversals_A2Z/PostorderTraversal.cpp
+++ Striver SDE Sheet/Day17_BinaryTree_Traversals_A2Z/PostorderTraversal.cpp
@@ -1,6 +1,7 @@
 // https://takeuforward.org/data-structure/post-order-traversal-of-binary-tree/
 // https://leetcode.com/problems/binary-tree-postorder-traversal/
 
+// 1. Recursive
 class Solution {
 public:
     void postorder(TreeNode* root, vector<int> &arr){
@@ -15,5 +16,37 @@ public:
         vector<int> arr;
         postorder(root, arr);
         return arr;
+    }
+};
+
+
+// 2. Iterative - using 2 stacks
+// https://www.youtube.com/watch?v=2YBhNLodD8Q&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=12
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s1, s2;
+        vector<int> postorder_traversal;
+        if(root==NULL){
+            return postorder_traversal;
+        }
+        s1.push(root);
+        while(s1.size()>0){
+            root = s1.top();
+            s1.pop();
+            s2.push(root);
+            if(root->left!=NULL){
+                s1.push(root->left);
+            }
+            if(root->right!=NULL){
+                s1.push(root->right);
+            }
+        }
+        while(s2.size()>0){
+            postorder_traversal.push_back(s2.top()->val);
+            s2.pop();
+        }
+        return postorder_traversal;
     }
 };
