9a8c5b77564383a456bd38dd2c238043a774713c
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-06-26 00:27:22 +0530

    Update PreorderTraversal.cpp

--- Striver SDE Sheet/Day17_BinaryTree_Traversals_A2Z/PreorderTraversal.cpp
+++ Striver SDE Sheet/Day17_BinaryTree_Traversals_A2Z/PreorderTraversal.cpp
@@ -1,6 +1,7 @@
 // https://takeuforward.org/data-structure/preorder-traversal-of-binary-tree/
 // https://leetcode.com/problems/binary-tree-preorder-traversal/
 
+// Recursive
 class Solution {
 public:
     void preorder(TreeNode* root, vector<int> &arr) {
@@ -15,5 +16,34 @@ public:
         vector<int> arr;
         preorder(root, arr);
         return arr;
+    }
+};
+
+
+// Iterative
+// https://www.youtube.com/watch?v=Bfqd8BsPVuw&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=10
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder_traversal;
+        if(root==NULL){
+            return preorder_traversal;
+        }
+
+        stack<TreeNode*> s;
+        s.push(root);
+        while(s.size()>0){
+            root=s.top();
+            s.pop();
+            preorder_traversal.push_back(root->val);
+            if(root->right!=NULL){
+                s.push(root->right);
+            }
+            if(root->left!=NULL){
+                s.push(root->left);
+            }
+        }
+        return preorder_traversal;
     }
 };
