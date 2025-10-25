042d79c826b74fd6b923dd6049cb8fc5fb070a95
Aayusha Bhatia <112889465+aayushabhatia@users.noreply.github.com>
2025-10-25 14:16:47 +0530

    Create Yt_Descending_Sorted_Array.cpp

---
+++ Striver SDE Sheet/Day11_BinarySearch/Yt_Descending_Sorted_Array.cpp
@@ -0,0 +1,22 @@
+// https://www.youtube.com/watch?v=YbkELwnGRdo&list=PL_z_8CaSLPWeYfhtuKHj-9MpYb6XQJ_f2&index=3
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size()-1;
+        while(start<=end){
+            int mid = start + ((end-start)/2);
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+};
